7df0b3bd3037ddd15c33fee43888443bb0cbfd28
Velmurugan Balasubramanian <velmurugan1124@gmail.com>
2021-06-03 21:36:13 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,19 +7,19 @@ rm -rf dist
 
 cd codelabs
 
-claat export 1kVNbCYhMC4OU2L0MoW_tM-S9ddrOv9gqJmcjCWnj6VY
+# claat export 1kVNbCYhMC4OU2L0MoW_tM-S9ddrOv9gqJmcjCWnj6VY
 
-claat export 1OikaA8dCJCzKSWWssl6RCaMZEngKU3vIX3eYuKalN0M
+# claat export 1OikaA8dCJCzKSWWssl6RCaMZEngKU3vIX3eYuKalN0M
 
-claat export 1zmeDhYAWRIjScyL792N_fU1x_Ar3vNrN275BEKzqYaA
+# claat export 1zmeDhYAWRIjScyL792N_fU1x_Ar3vNrN275BEKzqYaA
 
-claat export 1jqyv6SeAIykdKK-LOfdXSDTkxZeyU4hKySEIpH-yq_c
+# claat export 1jqyv6SeAIykdKK-LOfdXSDTkxZeyU4hKySEIpH-yq_c
 
-claat export 1k653X4ME0Y4jbwWEn7fu9mKe0gazhHR1vesNYUmPVvM
+# claat export 1k653X4ME0Y4jbwWEn7fu9mKe0gazhHR1vesNYUmPVvM
 
-claat export 1gtvwtWw9EyHNUTUhU7rtGr9qkt6y1m1DljM30yy6RdU
+# claat export 1gtvwtWw9EyHNUTUhU7rtGr9qkt6y1m1DljM30yy6RdU
 
-claat export 14T8CoZFKJa3VGqvk56gsl2Cubl-17F8XP6SBi_M7Xho
+# claat export 14T8CoZFKJa3VGqvk56gsl2Cubl-17F8XP6SBi_M7Xho
 
 # claat export ../content/app-setup-events/app-setup-and-ext-events-freshdesk.md
 
@@ -39,7 +39,38 @@ claat export 14T8CoZFKJa3VGqvk56gsl2Cubl-17F8XP6SBi_M7Xho
 
 # claat export ../content/introduction-to-freshworks-serverless-apps.md
 
+# App setup events 
+claat export ../content/app-setup-events/app-setup-and-ext-events-freshdesk.md
+
+# Data METHODS
+claat export ../content/data-methods/data-methods-freshdesk.md 
+
+# Data Storage and request
+claat export ../content/data-storage-and-request-method/data-storage-and-request-method.md 
+
+# Events Method
+claat export ../content/events-method/events-methods-freshdesk.md
+
+# Instance Methods
+claat export ../content/instance-methods/instance-methods.md
+
+# Interface Methods
+claat export ../content/interface-methods/interface-methods.md
+
+# lifecycle Events
+claat export ../content/lifecycle-events/lifecycle-and-configuration-page.md
+
+# Deep dive into serverless
+claat export ../content/deep-dive-into-serverless.md
+
+# Introduction to serverless
+claat export ../content/introduction-to-freshworks-serverless-apps.md
+
+pwd
+
 cd ../
+
+pwd
 
 # gulp dist
 
